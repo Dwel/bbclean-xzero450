5b0d40e6db53862b4352ff840b4a2e5ce869be92
Mojmir Svoboda <mojmir_svoboda@users.berlios.de>
2014-02-12 21:39:41 +0000

    * modified build dir

--- BlackboxZero/build_all_vs2012.bat
+++ BlackboxZero/build_all_vs2012.bat
@@ -1,9 +1,10 @@
 rem this file is intended for usage from cmd.exe or from explorer (clicking on it)
 @echo off
 
+set INSTDIR=c:/_builds/vs_vista_64
 mkdir _projects.vs11.64
 cd _projects.vs11.64
-cmake -G "Visual Studio 11 Win64" -DCMAKE_INSTALL_PREFIX:PATH=c:/bbZero.vs64 ..
+cmake -G "Visual Studio 11 Win64" -DCMAKE_INSTALL_PREFIX:PATH=%INSTDIR% ..
 if %errorlevel% neq 0 goto TERM
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86_amd64
 if %errorlevel% neq 0 goto TERM
@@ -12,9 +13,10 @@ if %errorlevel% neq 0 goto TERM
 
 cd ..
 
+set INSTDIR=c:/_builds/vs_vista_64_dbg
 mkdir _projects.vs11.64.dbg
 cd _projects.vs11.64.dbg
-cmake -G "Visual Studio 11 Win64" -DCMAKE_INSTALL_PREFIX:PATH=c:/bbZero.vs64.dbg ..
+cmake -G "Visual Studio 11 Win64" -DCMAKE_INSTALL_PREFIX:PATH=%INSTDIR% ..
 if %errorlevel% neq 0 goto TERM
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86_amd64
 if %errorlevel% neq 0 goto TERM
@@ -23,9 +25,10 @@ if %errorlevel% neq 0 goto TERM
 
 cd ..
 
+set INSTDIR=c:/_builds/vs_vista_32
 mkdir _projects.vs11.32
 cd _projects.vs11.32
-cmake -G "Visual Studio 11" -DCMAKE_INSTALL_PREFIX:PATH=c:/bbZero.vs32 ..
+cmake -G "Visual Studio 11" -DCMAKE_INSTALL_PREFIX:PATH=%INSTDIR% ..
 if %errorlevel% neq 0 goto TERM
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
 if %errorlevel% neq 0 goto TERM
@@ -34,9 +37,10 @@ if %errorlevel% neq 0 goto TERM
 
 cd ..
 
+set INSTDIR=c:/_builds/vs_vista_32_dbg
 mkdir _projects.vs11.32.dbg
 cd _projects.vs11.32.dbg
-cmake -G "Visual Studio 11" -DCMAKE_INSTALL_PREFIX:PATH=c:/bbZero.vs32.dbg ..
+cmake -G "Visual Studio 11" -DCMAKE_INSTALL_PREFIX:PATH=%INSTDIR% ..
 if %errorlevel% neq 0 goto TERM
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
 if %errorlevel% neq 0 goto TERM
@@ -45,8 +49,6 @@ if %errorlevel% neq 0 goto TERM
 
 cd ..
 
-rem devenv BlackBoxZero.sln [options] solutionconfig /project bbLeanSkin32
-rem devenv BlackBoxZero.sln [options] solutionconfig [/project projectnameorfile [/projectconfig name]]
 goto NOPAUSE
 
 :TERM
--- BlackboxZero/build_all_vs2012_xp.bat
+++ BlackboxZero/build_all_vs2012_xp.bat
@@ -3,9 +3,10 @@ rem this file is intended for usage from cmd.exe or from explorer (clicking on i
 
 echo 32b xp...
 
+set INSTDIR=c:/_builds/vs_xp_32
 mkdir _projects.vs11.32_xp
 cd _projects.vs11.32_xp
-cmake -G "Visual Studio 11" -T "v110_xp" -DCMAKE_INSTALL_PREFIX:PATH=c:/bbZero.vs32_xp ..
+cmake -G "Visual Studio 11" -T "v110_xp" -DCMAKE_INSTALL_PREFIX:PATH=%INSTDIR% ..
 if %errorlevel% neq 0 goto TERM
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
 if %errorlevel% neq 0 goto TERM
@@ -14,9 +15,10 @@ if %errorlevel% neq 0 goto TERM
 
 cd ..
 
+set INSTDIR=c:/_builds/vs_xp_32_dbg
 mkdir _projects.vs11.32_xp.dbg
 cd _projects.vs11.32_xp.dbg
-cmake -G "Visual Studio 11" -T "v110_xp" -DCMAKE_INSTALL_PREFIX:PATH=c:/bbZero.vs32_xp.dbg ..
+cmake -G "Visual Studio 11" -T "v110_xp" -DCMAKE_INSTALL_PREFIX:PATH=%INSTDIR% ..
 if %errorlevel% neq 0 goto TERM
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
 if %errorlevel% neq 0 goto TERM
@@ -27,9 +29,10 @@ cd ..
 
 echo 64b xp...
 
+set INSTDIR=c:/_builds/vs_xp_64
 mkdir _projects.vs11.64_xp
 cd _projects.vs11.64_xp
-cmake -G "Visual Studio 11 Win64" -T "v110_xp" -DCMAKE_INSTALL_PREFIX:PATH=c:/bbZero.vs64_xp ..
+cmake -G "Visual Studio 11 Win64" -T "v110_xp" -DCMAKE_INSTALL_PREFIX:PATH=%INSTDIR% ..
 if %errorlevel% neq 0 goto TERM
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86_amd64
 if %errorlevel% neq 0 goto TERM
@@ -37,18 +40,17 @@ devenv BlackBoxZero.sln /build  RelWithDebInfo /project INSTALL
 if %errorlevel% neq 0 goto TERM
 
 cd ..
-
+ 
+set INSTDIR=c:/_builds/vs_xp_64_dbg
 mkdir _projects.vs11.64_xp.dbg
 cd _projects.vs11.64_xp.dbg
-cmake -G "Visual Studio 11 Win64" -T "v110_xp" -DCMAKE_INSTALL_PREFIX:PATH=c:/bbZero.vs64_xp.dbg ..
+cmake -G "Visual Studio 11 Win64" -T "v110_xp" -DCMAKE_INSTALL_PREFIX:PATH=%INSTDIR% ..
 if %errorlevel% neq 0 goto TERM
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86_amd64
 if %errorlevel% neq 0 goto TERM
 devenv BlackBoxZero.sln /build Debug /project INSTALL
 if %errorlevel% neq 0 goto TERM
 
-rem devenv BlackBoxZero.sln [options] solutionconfig /project bbLeanSkin32
-rem devenv BlackBoxZero.sln [options] solutionconfig [/project projectnameorfile [/projectconfig name]]
 goto NOPAUSE
 
 :TERM
