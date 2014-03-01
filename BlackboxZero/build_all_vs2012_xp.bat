15929b0d4e051692740252012fec684b8b8bdcf5
Mojmir Svoboda <mojmir.svoboda@flogging.berlios.de>
2014-03-01 19:38:49 +0000

    * misc build fixes

--- BlackboxZero/build_all_vs2012_xp.bat
+++ BlackboxZero/build_all_vs2012_xp.bat
@@ -17,18 +17,6 @@ if %errorlevel% neq 0 goto TERM
 
 cd ..
 
-set INSTDIR=c:/_builds/vs_xp_32_dbg
-mkdir _projects.vs11.32_xp.dbg
-cd _projects.vs11.32_xp.dbg
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
-if %errorlevel% neq 0 goto TERM
-cmake -G "Visual Studio 11" -T "v110_xp" -DCMAKE_INSTALL_PREFIX:PATH=%INSTDIR% ..
-if %errorlevel% neq 0 goto TERM
-devenv BlackBoxZero.sln /build Debug /project INSTALL
-if %errorlevel% neq 0 goto TERM
-
-cd ..
-
 echo 64b xp...
 
 set INSTDIR=c:/_builds/vs_xp_64
@@ -43,16 +31,6 @@ if %errorlevel% neq 0 goto TERM
 
 cd ..
  
-set INSTDIR=c:/_builds/vs_xp_64_dbg
-mkdir _projects.vs11.64_xp.dbg
-cd _projects.vs11.64_xp.dbg
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86_amd64
-if %errorlevel% neq 0 goto TERM
-cmake -G "Visual Studio 11 Win64" -T "v110_xp" -DCMAKE_INSTALL_PREFIX:PATH=%INSTDIR% ..
-if %errorlevel% neq 0 goto TERM
-devenv BlackBoxZero.sln /build Debug /project INSTALL
-if %errorlevel% neq 0 goto TERM
-
 goto NOPAUSE
 
 :TERM
