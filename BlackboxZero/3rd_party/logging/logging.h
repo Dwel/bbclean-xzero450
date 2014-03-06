0814706fe662e4cdfcd394f18db0e8501dd248b9
Mojmir Svoboda <mojmir.svoboda@flogging.berlios.de>
2014-03-06 20:45:40 +0000

    * logging does not require submodule if logging not enabled

--- BlackboxZero/3rd_party/logging/logging.h
+++ BlackboxZero/3rd_party/logging/logging.h
@@ -1,12 +1,19 @@
 #pragma once
 
-#define TRACE_WINDOWS_SOCKET_FAILOVER_NOTIFY_MSVC 1
-#define TRACE_WINDOWS_SOCKET_FAILOVER_TO_FILE 1
-#define TRACE_CONFIG_INCLUDE "config.h"
-#define TRACE_LEVELS_INCLUDE "levels.h"
-#define TRACE_CONTEXTS_INCLUDE "contexts.h"
-#if !defined TRACE_STATIC
-# define TRACE_STATIC
-#endif
+#if defined TRACE_ENABLED
 
-#include <3rd_party/flogging/trace_client/trace.h>
+# define TRACE_WINDOWS_SOCKET_FAILOVER_NOTIFY_MSVC 1
+# define TRACE_WINDOWS_SOCKET_FAILOVER_TO_FILE 1
+# define TRACE_CONFIG_INCLUDE "config.h"
+# define TRACE_LEVELS_INCLUDE "levels.h"
+# define TRACE_CONTEXTS_INCLUDE "contexts.h"
+# if !defined TRACE_STATIC
+#   define TRACE_STATIC
+# endif
+
+# include <3rd_party/flogging/trace_client/trace.h>
+
+#else
+// no tracing at all
+# include "trace_dummy.h"
+#endif
