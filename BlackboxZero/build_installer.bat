5b02d473773e3c612ca9cbacb5a3ab23941406e8
Mojmir Svoboda <mojmir.svoboda@bistudio.com>
2014-08-13 18:02:54 +0200

    + install.exe renamed to me (for now) as builder

--- BlackboxZero/build_installer.bat
+++ BlackboxZero/build_installer.bat
@@ -7,7 +7,7 @@ set OUTDIR=c:/_builds
 echo copying NSIS installer files...
 
 robocopy.exe installer %OUTDIR% blackbox.nsi installer.bmp 
-robocopy.exe . %OUTDIR% GPL.txt
+robocopy.exe . %OUTDIR% GPL.txt build.ver
 robocopy.exe 3rd_party\redistributables\2012u4 %OUTDIR%\redist vcredist_x86.exe vcredist_x64.exe
 rem if %errorlevel% neq 0 goto TERM
 
@@ -18,6 +18,14 @@ set PATH=C:\Program Files (x86)\NSIS\unicode;%PATH%
 cd %OUTDIR%
 makensis.exe blackbox.nsi
 rem makensis.exe /X"SetCompressor /FINAL lzma" myscript.nsi
+if %errorlevel% neq 0 goto TERM
+
+echo Renaming executable...
+
+set /p VERSION=< build.ver
+set INSTALLER_NAME="bbZero_install_%VERSION%_mojmir.exe"
+echo New installer name: %INSTALLER_NAME%
+ren install.exe %INSTALLER_NAME%
 if %errorlevel% neq 0 goto TERM
 
 goto NOPAUSE
